0a7eb62153751bfd71ccdfe0b06aa6d734b4ac35
chrismrkr <b635032_@daum.net>
2026-09-01 23:24:37 +0900

    edit: 생성형 AI 호출 결과 로깅을 위한 임시 커밋

--- src/main/java/kko/traveldiary_api/city/application/CityRegistrationService.java
+++ src/main/java/kko/traveldiary_api/city/application/CityRegistrationService.java
@@ -9,6 +9,7 @@ import kko.traveldiary_api.city.domain.CityGenerateRequest;
 import kko.traveldiary_api.city.domain.CityImage;
 import kko.traveldiary_api.shared.Coordinate;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +18,7 @@ import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CityRegistrationService implements CityRegistration, CityDetailRegistration {
     private final CityRepository repository;
     private final CityRegistrationEventPublisher eventPublisher;
@@ -50,6 +52,13 @@ public class CityRegistrationService implements CityRegistration, CityDetailRegi
 
             CityDescription cityDescription = cityDescriptionGenerator.generate(city);
             CityImage image = imageGenerator.generate(cityDescription);
+
+            log.debug("[CityDescription funFact] {}", cityDescription.funFact());
+            log.debug("[CityDescription historyAndCulture] {}", cityDescription.historyAndCulture());
+            log.debug("[CityDescription localTip] {}", cityDescription.localTip());
+            log.debug("[CityDescription overview] {}", cityDescription.overview());
+
+            log.debug("[Image Bytes] {}", image.imageBytes());
 
             imageStorage.save(image.id(), image.imageBytes());
 
